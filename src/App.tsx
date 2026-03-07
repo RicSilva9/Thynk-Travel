9149f8c22891102e45a71cd6db3b1c155159cd87
Ricardo da Silva Sousa <rssricardo82@gmail.com>
2026-03-07 11:19:49 -0300

    chore: setup project base with Vite + React + TypeScript + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-sky-600 text-white text-4xl font-bold">
+      Thynk Travel ✈️
+    </main>
+  )
+}
+
+export default App
